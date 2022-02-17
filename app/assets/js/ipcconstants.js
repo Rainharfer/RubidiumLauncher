080a96eea8bf41d10d599f62e6332f5d6567eee9
Rainharfer <68148232+Rainharfer@users.noreply.github.com>
2022-02-17 17:15:54 -0500

    Initial Setup

--- app/assets/js/ipcconstants.js
+++ app/assets/js/ipcconstants.js
@@ -1,7 +1,7 @@
 // NOTE FOR THIRD-PARTY
 // REPLACE THIS CLIENT ID WITH YOUR APPLICATION ID.
 // SEE https://github.com/dscalzi/HeliosLauncher/blob/master/docs/MicrosoftAuth.md
-exports.AZURE_CLIENT_ID = '1ce6e35a-126f-48fd-97fb-54d143ac6d45'
+exports.AZURE_CLIENT_ID = '8f61a753-a3a8-41ce-beca-920f77c7beed'
 // SEE NOTE ABOVE.
 
 
